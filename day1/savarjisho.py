a66ea13330ea4fa17acc92afe40f8d2d218923d7
sannnnn5 <aleksandregoguadze@gmail.com>
2022-10-06 21:05:03 +0400

    xmovnebi da tanxmovnebi

--- day1/savarjisho.py
+++ day1/savarjisho.py
@@ -45,19 +45,19 @@
 #    print("my_sentency2-shi {} tanxmovania".format(sum_2))        
 
 
-my_sentency=input("enter: ")
-my_sentency2=input("enter 2: ")
+my_name1=input("enter: ")
+my_name2=input("enter 2: ")
 sum_1=0
 sum_2=0
-for i in my_sentency:
+for i in my_name1:
    if i not in "aeiou":
      sum_1+=1
 
-for i in my_sentency2:
+for i in my_name2:
    if i not in "aeiou":
      sum_2+=1
 if sum_1>sum_2:
-   print("my_sentency1-shi {} tanxmovania".format(sum_1))     
+   print("my_name1-ში {} თანხმოვანია".format(sum_1))     
 if sum_2>sum_1:
-   print("my_sentency2-shi {} tanxmovania".format(sum_2))        
+   print("my_name2-ში {} თანხმოვანია".format(sum_2))        
 
